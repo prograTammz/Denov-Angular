f2e225596d87377f21d58f98720c934cc057cfe0
prograTammz <ahmedtamer1998@gmail.com>
2019-07-18 21:58:38 +0200

    Added Routing Module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes, Router} from '@angular/router';
+
+const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: , data:{routeName:"Denov.Co"}},
+    {path: 'commingSoon', component: , data:{routeName:"Denov.Co"}},
+    {path: 'trucking', component: , data:{routeName:"Trucking"}},
+    {path: 'market', component: , data:{routeName:"Market"}},
+    {path: 'dcurrency', component: , data:{routeName:"D-Currency"}},
+    {path: 'eservices', component: , data:{routeName:"E-services"}},
+    {path: 'bank', component: , data:{routeName:"Bank"}},
+    {path: '', component: , data:{routeName:"Denov.Co"}},
+    {path: '**', component:, data:{routeName:"Erroooor"}}
+]
+@NgModule({
+    declarations: [],
+    imports: [
+      CommonModule,RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
+  })
+export class AppRoutingModule{}
