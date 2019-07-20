f5ddd3c5672de1aa5cad99cd174cba2f21e9147d
prograTammz <ahmedtamer1998@gmail.com>
2019-07-20 14:37:36 +0200

    Adding back button

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
 
+import {Location} from '@angular/common';
+import { NavigationEnd,Router,ActivatedRoute,RouterEvent } from '@angular/router';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -10,7 +13,8 @@ import {DomSanitizer} from '@angular/platform-browser';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor(private sanitizer: DomSanitizer,private matIconRegistry: MatIconRegistry,) { 
+  public title: string;
+  constructor(private sanitizer: DomSanitizer,private matIconRegistry: MatIconRegistry,private router: Router,private location: Location, private activatedRoute: ActivatedRoute) {
     this.matIconRegistry.addSvgIcon('menu', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/menu.svg'));
     this.matIconRegistry.addSvgIcon('close', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/close.svg'));
   }
@@ -18,4 +22,7 @@ export class NavigationComponent implements OnInit {
   ngOnInit() {
   }
 
+  goBack():void{
+    this.location.back();
+  }
 }
