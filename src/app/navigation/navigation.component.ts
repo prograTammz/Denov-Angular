f1feedd0a3a39d492c3edbbbdccd224729fe9013
prograTammz <ahmedtamer1998@gmail.com>
2019-07-21 21:12:52 +0200

    Side nav closes when a router is clicked

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 
 import { MatIconRegistry } from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
 
 import {Location} from '@angular/common';
 import { NavigationEnd,Router,ActivatedRoute,RouterEvent } from '@angular/router';
-
+import {MatSidenav} from '@angular/material/sidenav'
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -13,13 +14,21 @@ import { NavigationEnd,Router,ActivatedRoute,RouterEvent } from '@angular/router
 })
 export class NavigationComponent implements OnInit {
 
+  @ViewChild(MatSidenav, {static: false}) private sideNav: MatSidenav;
+  private navigationEnd: Observable<RouterEvent>;
   public title: string;
+
   constructor(private sanitizer: DomSanitizer,private matIconRegistry: MatIconRegistry,public router: Router,private location: Location, private activatedRoute: ActivatedRoute) {
     this.matIconRegistry.addSvgIcon('menu', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/menu.svg'));
     this.matIconRegistry.addSvgIcon('close', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/close.svg'));
   }
 
   ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.sideNav.close();
+      }
+    });
   }
 
   goBack():void{
