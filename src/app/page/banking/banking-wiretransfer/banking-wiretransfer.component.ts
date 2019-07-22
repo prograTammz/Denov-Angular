de48789ba431e7a7d781c40086f333524c5d2537
prograTammz <ahmedtamer1998@gmail.com>
2019-07-22 08:12:16 +0200

    Added Wire transfer form

--- src/app/page/banking/banking-wiretransfer/banking-wiretransfer.component.ts
+++ src/app/page/banking/banking-wiretransfer/banking-wiretransfer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-banking-wiretransfer',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banking-wiretransfer.component.css']
 })
 export class BankingWiretransferComponent implements OnInit {
+  wireTransfer: FormGroup = new FormGroup({
+      firstName:  new FormControl(''),
+      lastName:  new FormControl(''),
+      amount:  new FormControl(''),
+  })
+   
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log(this.wireTransfer.value);
+  }
 }
