42844d43d8e71208818aa9416e125f15db9199ae
prograTammz <ahmedtamer1998@gmail.com>
2019-07-22 12:51:16 +0200

    Page will navigate up when route changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {FadeAnimation} from './route-animation';
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [FadeAnimation]
 })
-export class AppComponent {
-  title = 'Denov-Angular';
+export class AppComponent implements OnInit {
+  title = 'Denov.co';
+  constructor(public router: Router){
+
+  }
+  ngOnInit(){
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      window.scrollTo(0, 0)
+  });
+  }
 }
 
