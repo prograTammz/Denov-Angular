61fd66fd7b1c40b69f6cc7b9a2c64127debd8525
prograTammz <ahmedtamer1998@gmail.com>
2019-08-05 21:12:33 +0200

    added user interface

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,19 @@
+export interface User {
+    id: string,
+    firstName:  string,
+    lastName:   string,
+    isSuperAdmin: boolean,
+    isAdmin: boolean,
+    isOfficer: boolean,
+    isTruckerHR: boolean,
+    isBanker: boolean,
+    isTrucker: boolean,
+    isSaver: boolean,
+    isLoaner: boolean,
+    isInvestor: boolean,
+    isDriver: boolean,
+    isMiner: boolean,
+    isSeller: boolean,
+    isRecruiter: boolean,
+    email: string
+}
