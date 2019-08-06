5eb7b3d75139cf5f2a3253e154b13947c51373e8
prograTammz <ahmedtamer1998@gmail.com>
2019-08-06 12:53:31 +0200

    added isAuth observable to auth service

--- src/app/services/api/auth.service.ts
+++ src/app/services/api/auth.service.ts
@@ -8,10 +8,14 @@ import { map } from 'rxjs/operators';
 export class AuthService {
 
   private userSubject: BehaviorSubject<any>;
+  private isAuthSubject: BehaviorSubject<boolean>;
   public User: Observable<any>;
+  public isAuth: Observable<boolean>;
   constructor(private http: HttpClient) {
     this.userSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('user')));
+    this.isAuthSubject = new BehaviorSubject<boolean>(Boolean(localStorage.getItem('isAuth')));
     this.User = this.userSubject.asObservable();
+    this.isAuth = this.isAuthSubject.asObservable();
   }
 
   public login(email: string, password: string){
@@ -24,6 +28,8 @@ export class AuthService {
               }
               localStorage.setItem('token',JSON.stringify(tempUser));
               this.userSubject.next(tempUser);
+              localStorage.setItem('isAuth','true');
+              this.isAuthSubject.next(true);
             }
             return user;
           }))
