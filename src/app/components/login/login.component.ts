e80f889d36133a7069446b2dbe5a48daa1a62e08
prograTammz <ahmedtamer1998@gmail.com>
2019-08-06 16:07:33 +0200

    removed console.log testing

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -22,19 +22,15 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
   onSubmit() {
-    console.log(this.login.value);
     this.isLoading = true;
     this.auth.login(this.login.value.email,this.login.value.password)
       .pipe(first())
       .subscribe(
         data =>{
-          console.log(data);
           this._snackBar.open("Sucessfully loggged in !", 'close', {duration: 2000})
           this.closeDialog();
         },
         error =>{
-          console.error(error);
-          console.log(error.error);
           this._snackBar.open(error.error, 'close', {duration:2000})
           this.isLoading = false;
         }
