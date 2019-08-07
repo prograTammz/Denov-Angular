ffe65fd8b8500ec4b7362d48df5b9ea681392a05
prograTammz <ahmedtamer1998@gmail.com>
2019-08-07 20:12:05 +0200

    added more news api service functions

--- src/app/services/api/news.service.ts
+++ src/app/services/api/news.service.ts
@@ -1,17 +1,51 @@
 import { Injectable } from '@angular/core';
 import {ConfigService} from './config.service';
 import { Config } from 'src/app/models/config.model';
-
+import { News } from 'src/app/models/news.model';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { first, map } from 'rxjs/operators';
+import { UserStorageService } from '../storage/user.service';
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  private url: string;
-  constructor(private config: ConfigService,) {
-    config.getConfig().subscribe((data: Config) =>{
+  url: string;
+  private httpOptions: object;
+  private headers: HttpHeaders;
+  constructor(private config: ConfigService,private http: HttpClient, private user: UserStorageService) {
+   this.config.getConfig().subscribe((data: Config) =>{
       this.url = data.url;
     })
+    this.headers = new HttpHeaders({
+      'x-auth-token': this.user.getUser().token
+    })
+    this.httpOptions={
+      headers: this.headers
+    }
    }
-   
+   getNews(page: number, size: number): Observable<News[]>{
+      return this.http.get(`${this.url}/api/news?pageNo=${page}&${size}`)
+      .pipe(
+        first(),
+        map((news: News[])=>{
+          return news;
+        }))
+   }
+   public postNews(title: string, body: string): Observable<any>{
+     const news = {
+       title: title,
+       body: body
+     }
+     return this.http.post(`${this.url}/api/news`,news,this.httpOptions)
+   }
+   public getCount():Observable<Number>{
+     return this.http.get(`${this.url}/api/news/count`)
+      .pipe(
+        first(),
+        map((count: number)=>{
+          return count;
+      }))
+   }
 }
