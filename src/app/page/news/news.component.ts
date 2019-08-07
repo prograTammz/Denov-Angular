08c52d194e52f135a489c676241d3ece8f4f8259
prograTammz <ahmedtamer1998@gmail.com>
2019-08-07 18:08:46 +0200

    added init content to news page

--- src/app/page/news/news.component.ts
+++ src/app/page/news/news.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {PageEvent} from '@angular/material/paginator';
+import {UserStorageService} from '../../services/storage/user.service';
+import {User} from '../../models/user.model';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  length:number;
+  pageSize:number
+  pageSizeOptions: number[];
+  pageEvent: PageEvent;
+  isAuthorized: boolean;
+  user: User;
+  constructor(private userStorage: UserStorageService) {
+    this.isAuthorized = false;
+   }
 
   ngOnInit() {
+    this.length = 100;
+    this.pageSize = 10;
+    this.pageSizeOptions = [5,10,25,100];
+    this.user = this.userStorage.getUser();
+    if(this.user){
+      if(this.user.isAdmin || this.user.isSuperAdmin){
+        this.isAuthorized = true
+      }
+    }
+    
   }
 
 }
