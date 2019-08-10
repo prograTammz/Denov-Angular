6305a493b10e6bca826766edc737f05f2013cea0
prograTammz <ahmedtamer1998@gmail.com>
2019-08-10 12:35:40 +0200

    added functionality to install button

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -10,6 +10,7 @@ import { Observable, fromEventPattern } from 'rxjs';
 
 import {AuthService} from '../services/api/auth.service';
 import {LoginDialogService} from '../services/dialogs/login-dialog.service';
+import {InstallDialogService} from '../services/dialogs/install-dialog.service';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -22,7 +23,7 @@ export class NavigationComponent implements OnInit {
   public title: string;
   public isAuth;
   public isInstalled: boolean;
-  constructor(private auth: AuthService,private loginDialog: LoginDialogService,private sanitizer: DomSanitizer,private matIconRegistry: MatIconRegistry,public router: Router,private location: Location, private activatedRoute: ActivatedRoute) {
+  constructor(private installDialog: InstallDialogService,private auth: AuthService,private loginDialog: LoginDialogService,private sanitizer: DomSanitizer,private matIconRegistry: MatIconRegistry,public router: Router,private location: Location, private activatedRoute: ActivatedRoute) {
     this.matIconRegistry.addSvgIcon('menu', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/menu.svg'));
     this.matIconRegistry.addSvgIcon('close', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/close.svg'));
     this.isAuth = false;
@@ -51,5 +52,8 @@ export class NavigationComponent implements OnInit {
   }
   logout(){
     this.auth.logout();
+  }
+  openInstallDialog(){
+    this.installDialog.openModal();
   }
 }
