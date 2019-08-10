4482f7863f2002d79b6e15032dfbaffcfb215b1e
prograTammz <ahmedtamer1998@gmail.com>
2019-08-10 14:54:17 +0200

    inject news modal services and added functionality to the button

--- src/app/page/news/news.component.ts
+++ src/app/page/news/news.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked} from '@angular/core';
 import {PageEvent} from '@angular/material/paginator';
 import {UserStorageService} from '../../services/storage/user.service';
 import {User} from '../../models/user.model';
+import {NewsService} from '../../services/api/news.service';
+import { AddNewsDialogService } from 'src/app/services/dialogs/add-news-dialog.service';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
@@ -15,7 +17,7 @@ export class NewsComponent implements OnInit {
   pageEvent: PageEvent;
   isAuthorized: boolean;
   user: User;
-  constructor(private userStorage: UserStorageService) {
+  constructor(private newsForm: AddNewsDialogService,private userStorage: UserStorageService, private news: NewsService) {
     this.isAuthorized = false;
    }
 
@@ -29,7 +31,16 @@ export class NewsComponent implements OnInit {
         this.isAuthorized = true
       }
     }
-    
-  }
+    this.news.getCount().subscribe(
+      count =>{
+        console.log(count);
+      },
+      error=>{
 
+      }
+    )
+  }
+  openDialog(){
+    this.newsForm.openModal();
+  }
 }
