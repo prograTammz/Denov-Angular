6d2a7637b8358edf852c3fa07de9e6d7b58ac655
prograTammz <ahmedtamer1998@gmail.com>
2019-08-10 16:39:52 +0200

    forms send the data to the API

--- src/app/components/news-form/news-form.component.ts
+++ src/app/components/news-form/news-form.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {NewsService} from '../../services/api/news.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import { MatDialogRef } from '@angular/material/dialog';
+import { first } from 'rxjs/operators';
 @Component({
   selector: 'app-news-form',
   templateUrl: './news-form.component.html',
@@ -20,6 +21,21 @@ export class NewsFormComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+  onSubmit() {
+    this.isLoading = true;
+    this.news.postNews(this.new.value.title,this.new.value.body)
+    .pipe(first())
+    .subscribe(
+      data=>{
+        this._snackBar.open("Sucessfully published", 'close', {duration: 2000})
+        this.closeDialog();
+      },
+      error=>{
+        this._snackBar.open(error.error, 'close', {duration:2000})
+        this.isLoading = false;
+      }
+    )
   }
   closeDialog(): void{
     this.dialogRef.close();
