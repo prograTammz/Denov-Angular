5a651ebc3e3217624cb744a83c11b0dae7357783
prograTammz <ahmedtamer1998@gmail.com>
2019-08-19 17:12:35 +0200

    added routing to detail page, animation improvment

--- src/app/page/priceguide/vehicle-list/vehicle-list.component.ts
+++ src/app/page/priceguide/vehicle-list/vehicle-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-vehicle-list',
@@ -19,7 +20,7 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
       })),
       transition('normal=>enlarge',[
         style({display: 'block', opacity: '0'}),
-        animate('300ms', style({opacity: '1', top: '16px',left:'16px', width:'calc(100% - 32px)'}))
+        animate('400ms', style({opacity: '1', top: '16px',left:'16px', width:'calc(100% - 32px)'}))
       ])
     ])
   ]
@@ -28,24 +29,34 @@ export class VehicleListComponent implements OnInit {
 
   brands: number[]
   isAnimated: boolean
-  constructor() { }
+  constructor(public router: Router, private activatedRoute: ActivatedRoute) {
+    
+   }
 
   ngOnInit() {
     this.brands = [1,2,3,4,5,6,7,8];
     this.isAnimated = false;
   }
-  toggle(img, event){
-    console.log(event);
-    let elm: any = document.getElementById('animate-image');
-    elm.src = img;
-    console.log(event.clientX);
-    elm.style.top = event.clientY+"px";
-    elm.style.left = event.clientX+"px";
-    console.log(document.getElementById('animate-image'));
-    console.log(elm);
+  toggle(img:HTMLImageElement, event, placeHolder: HTMLElement){
+    let elm: HTMLImageElement = document.getElementById('animate-image') as HTMLImageElement;
+    let parentElm: HTMLElement = img.offsetParent as HTMLElement;
+    elm.src = img.src;
+
+    placeHolder.style.backgroundColor = "#ffffff";
+    placeHolder.style.height = img.offsetHeight+'px';
+    placeHolder.style.width = img.offsetWidth+'px';
+    img.style.display = 'none';
+    
+    elm.style.width = parentElm.offsetWidth+'px';
+    window.scrollTo({top:0, behavior: 'smooth'});
+    elm.style.top = parentElm.offsetTop+"px";
+    elm.style.left = parentElm.offsetWidth+"px";
     this.isAnimated = !this.isAnimated;
   }
   animationEnd(){
-    console.log('HEY')
+    if(this.isAnimated){
+      this.router.navigate(['/vehicle','detail']);
+    }
+    
   }
 }
