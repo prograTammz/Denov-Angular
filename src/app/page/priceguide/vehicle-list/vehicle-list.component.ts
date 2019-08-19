eeba61585f9972d55515d677ca61258edb37bc8a
prograTammz <ahmedtamer1998@gmail.com>
2019-08-19 16:25:46 +0200

    added angular animation when moving to detail page

--- src/app/page/priceguide/vehicle-list/vehicle-list.component.ts
+++ src/app/page/priceguide/vehicle-list/vehicle-list.component.ts
@@ -1,17 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-vehicle-list',
   templateUrl: './vehicle-list.component.html',
-  styleUrls: ['./vehicle-list.component.css']
+  styleUrls: ['./vehicle-list.component.css'],
+  animations: [
+    trigger('photoState',[
+      state('enlarge', style({
+        display:'block',
+        opacity: '1',
+         top: '16px',
+         left:'16px',
+          width:'calc(100% - 32px)'
+      })),
+      state('normal',style({
+        display: 'none'
+      })),
+      transition('normal=>enlarge',[
+        style({display: 'block', opacity: '0'}),
+        animate('300ms', style({opacity: '1', top: '16px',left:'16px', width:'calc(100% - 32px)'}))
+      ])
+    ])
+  ]
 })
 export class VehicleListComponent implements OnInit {
 
-  brands: string[]
+  brands: number[]
+  isAnimated: boolean
   constructor() { }
 
   ngOnInit() {
-    this.brands = [,,,,,,,]
+    this.brands = [1,2,3,4,5,6,7,8];
+    this.isAnimated = false;
   }
-
+  toggle(img, event){
+    console.log(event);
+    let elm: any = document.getElementById('animate-image');
+    elm.src = img;
+    console.log(event.clientX);
+    elm.style.top = event.clientY+"px";
+    elm.style.left = event.clientX+"px";
+    console.log(document.getElementById('animate-image'));
+    console.log(elm);
+    this.isAnimated = !this.isAnimated;
+  }
+  animationEnd(){
+    console.log('HEY')
+  }
 }
