fd683b73337e2ee2df67b35bf22f2498c86c2cb0
prograTammz <ahmedtamer1998@gmail.com>
2020-05-30 14:54:52 +0200

    the side nav overlays incase of mobile

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -4,13 +4,23 @@ import { SideNavService } from './side-nav.service';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
 export class SideNavComponent implements OnInit {
 
 
   @ViewChild(MatSidenav, {static: false}) public sideNav: MatSidenav;
-  constructor(private sidenavService: SideNavService) { }
+  
+  public viewWidth: number;
+  public isDesktop: boolean;
+  
+  constructor(private sidenavService: SideNavService) { 
+    this.viewWidth = window.innerWidth;
+    this.isDesktop = false;
+  }
 
 
   ngOnInit(): void {
@@ -19,5 +29,8 @@ export class SideNavComponent implements OnInit {
   ngAfterViewInit(): void {
     this.sidenavService.setSidenav(this.sideNav);
   }
-
+  onResize(event) {
+    this.viewWidth = event.target.innerWidth;
+    this.isDesktop = false;
+  }
 }
