1254a987fe9ba8bc7a3e7a3c0996c60f09fcdbcb
prograTammz <ahmedtamer1998@gmail.com>
2020-05-30 13:25:21 +0200

    added contact us routing

--- src/app/page/info/info-routing.module.ts
+++ src/app/page/info/info-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'aboutus', pathMatch: 'full'},
-  {path:'aboutus', component: AboutusComponent}
+  {path:'aboutus', component: AboutusComponent},
+  {path:'contactus',component: ContactusComponent}
   ];
   
   @NgModule({
