930a8d909dc0f31579a22a5f6160354291a789c2
prograTammz <ahmedtamer1998@gmail.com>
2020-05-30 14:16:57 +0200

    Fixed side nav incaseof desktop

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, HostListener} from '@angular/core';
 
 import { MatIconRegistry } from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
@@ -17,7 +17,10 @@ import {SideNavService} from '../components/side-nav/side-nav.service';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
 export class NavigationComponent implements OnInit {
 
@@ -26,28 +29,45 @@ export class NavigationComponent implements OnInit {
   public title: string;
   public isAuth;
   public isInstalled: boolean;
+  public viewWidth: number;
+  public isDesktop: boolean;
   constructor(private sideNav:SideNavService, private auth: AuthService,private loginDialog: LoginDialogService,private sanitizer: DomSanitizer,private matIconRegistry: MatIconRegistry,public router: Router,private location: Location, private activatedRoute: ActivatedRoute) {
     this.matIconRegistry.addSvgIcon('menu', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/menu.svg'));
     this.matIconRegistry.addSvgIcon('close', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/close.svg'));
     this.matIconRegistry.addSvgIcon('burger-menu',sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/burger-icon.svg'));
     this.isAuth = false;
     this.isInstalled = false;
+    this.viewWidth = window.innerWidth;
+    this.isDesktop = false;
   }
 
   ngOnInit() {
+    
     this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
+      if (event instanceof NavigationEnd && !this.isDesktop) {
         this.sideNav.close();
       }
     });
+    this.checkSideNav();
     this.auth.isAuth.subscribe(auth=>{
       this.isAuth = auth;
     });
-    if (window.matchMedia('(display-mode: standalone)').matches) {  
-      this.isInstalled = true;
-    }
+    
   }
 
+  onResize(event) {
+    this.viewWidth = event.target.innerWidth;
+    this.isDesktop = false;
+    this.sideNav.close();
+    this.checkSideNav();
+  }
+
+  checkSideNav(){
+    if(this.viewWidth > 1050){
+      this.isDesktop = true;
+      this.sideNav.open();
+    }
+  }
   goBack():void{
     this.location.back();
   }
