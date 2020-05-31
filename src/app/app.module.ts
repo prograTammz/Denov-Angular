0540c94ae7cce8b94b91d8ebd32fe99c295bb369
prograTammz <ahmedtamer1998@gmail.com>
2020-05-31 05:51:07 +0200

    reorganized and commented modules in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//angular modules to be moved
 import {ReactiveFormsModule} from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
+import { environment } from '../environments/environment';
+
+
+//angular material modules
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
@@ -12,35 +19,42 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatInputModule} from '@angular/material/input';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatExpansionModule} from '@angular/material/expansion';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatRippleModule} from '@angular/material/core'
+
+//angular material modules to be moved to other modules
+import {MatInputModule} from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
 import {MatPaginatorModule} from '@angular/material/paginator';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './navigation/navigation.component';
 
-
-
+//appshell modules
 import { HomeComponent } from './page/home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+//placeholder pages
+import { NotfoundComponent } from './page/notfound/notfound.component';
+import { SoonComponent } from './page/soon/soon.component';
+
+//to be moved to userModule
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './page/register/register.component';
+
+//to be moved to news Module
 import { NewsComponent } from './page/news/news.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { NewsFormComponent } from './components/news-form/news-form.component';
+
+//to be moved to priceGuide Module
 import { VehicleBrandComponent } from './page/priceguide/vehicle-brand/vehicle-brand.component';
 import { VehicleListComponent } from './page/priceguide/vehicle-list/vehicle-list.component';
 import { VehicleDetailComponent } from './page/priceguide/vehicle-detail/vehicle-detail.component';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
-import { NotfoundComponent } from './page/notfound/notfound.component';
-import { SoonComponent } from './page/soon/soon.component';
+
+
 
 
 @NgModule({
