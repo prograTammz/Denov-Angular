c5fbba69c7acb2373c4abd44559fa6d184ce5cc3
prograTammz <ahmedtamer1998@gmail.com>
2020-06-01 12:19:06 +0200

    Added contacus and abotu us routing

--- src/app/page/information/information-routing.module.ts
+++ src/app/page/information/information-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CareerComponent } from './career/career.component';
 import { InvestComponent } from './invest/invest.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 
 const routes: Routes = [
-  //{path: '', redirectTo: 'aboutus', pathMatch: 'full'},
+  {path: '', redirectTo: 'contactus', pathMatch: 'full'},
   { path: 'career', component: CareerComponent },
-  { path:'invest', component:InvestComponent}
+  { path:'invest', component:InvestComponent},
+  {path:'aboutus', component: AboutusComponent},
+  {path:'contactus', component: ContactusComponent},
 ];
 
 @NgModule({
