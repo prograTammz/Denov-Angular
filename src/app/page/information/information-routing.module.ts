f5951f912bc764ca34dc94ced9bc30a5c11937ba
prograTammz <ahmedtamer1998@gmail.com>
2020-06-01 12:12:37 +0200

    Added investment page routing

--- src/app/page/information/information-routing.module.ts
+++ src/app/page/information/information-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CareerComponent } from './career/career.component';
+import { InvestComponent } from './invest/invest.component';
 
 
 const routes: Routes = [
   //{path: '', redirectTo: 'aboutus', pathMatch: 'full'},
-  { path: 'career', component: CareerComponent }
+  { path: 'career', component: CareerComponent },
+  { path:'invest', component:InvestComponent}
 ];
 
 @NgModule({
