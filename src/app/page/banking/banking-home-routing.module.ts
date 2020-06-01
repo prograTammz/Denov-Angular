41c7c497b6eab88c3a431e17e64d04bec747e3b9
prograTammz <ahmedtamer1998@gmail.com>
2020-06-01 12:30:20 +0200

    Added Banking fees page to routing and modified sideMenu

--- src/app/page/banking/banking-home-routing.module.ts
+++ src/app/page/banking/banking-home-routing.module.ts
@@ -5,13 +5,15 @@ import {BankingHomeComponent} from './banking-home/banking-home.component';
 import {BankingTermsComponent}          from './banking-terms/banking-terms.component';
 import {BankingWiretransferComponent}   from './banking-wiretransfer/banking-wiretransfer.component';
 import { BankingPlansComponent }        from './banking-plans/banking-plans.component';
+import { BankingFeesComponent } from './banking-fees/banking-fees.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'overview', pathMatch: 'full'},
   {path:'overview', component: BankingHomeComponent},
   {path:'terms-conditions', component:BankingTermsComponent},
   {path:'wire-transfer',component:BankingWiretransferComponent},
-  {path: 'plans',component:BankingPlansComponent}
+  {path: 'plans',component:BankingPlansComponent},
+  {path: 'fees', component:BankingFeesComponent}
   ];
   
   @NgModule({
