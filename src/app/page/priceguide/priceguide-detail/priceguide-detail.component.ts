78290ea2deb0ebe685a4de4b37016cc8f40679b2
prograTammz <ahmedtamer1998@gmail.com>
2020-06-09 19:42:32 +0200

    added upgrades pricing

--- src/app/page/priceguide/priceguide-detail/priceguide-detail.component.ts
+++ src/app/page/priceguide/priceguide-detail/priceguide-detail.component.ts
@@ -33,8 +33,36 @@ export class PriceguideDetailComponent implements OnInit {
   loading = true;
   error: any;
   id: number;
+  upgrades: any[];
   constructor(private apollo: Apollo,private activatedroute:ActivatedRoute) { 
+    this.upgrades=[
+      {name:"Lock 2nd Generation",price:3000},
+      {name:"Lock 3rd Generation", price:6000},
+      {name:"Alarm 2nd Generation", price:5000},
+      {name:"Alarm 3rd Generation", price:10000},
+      {name:"GPS", price:500},
+      {name:"Respray", price:1250},
+      {name:"Spoiler", price:2000},
+      {name:"Front Bumper", price:1600},
+      {name:"Side Skirt", price:1200},
+      {name:"Exhaust", price:1500},
+      {name:"Frame", price:2000},
+      {name:"Grille", price:1250},
+      {name:"Hood", price:2000},
+      {name:"Fenders", price:1500},
+      {name:"Roof", price:2000},
+      {name:"Engine", price:8000},
+      {name:"Brakes", price:5000},
+      {name:"Transmission", price:2000},
+      {name:"Suspension", price:5000},
+      {name:"Wheels", price:750},
+      {name:"Liverys", price:3500},
+      {name:"Plate", price:5000},
+      {name:"Turbo", price:9000},
+      {name:"Turbo", price:400},
+      {name:"Window Tint", price:200},
 
+    ]
   }
 
   ngOnInit(): void {
