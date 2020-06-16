670b1e61d545bed5b544c151c8b232d7de1d2f69
prograTammz <ahmedtamer1998@gmail.com>
2020-06-16 08:40:57 +0200

    Fixes and changes in the priceguide

--- src/app/page/priceguide/priceguide-detail/priceguide-detail.component.ts
+++ src/app/page/priceguide/priceguide-detail/priceguide-detail.component.ts
@@ -34,12 +34,18 @@ export class PriceguideDetailComponent implements OnInit {
   error: any;
   id: number;
   upgrades: any[];
+  maintance: any[];
+  displayedColumns: string[] = ['mileage','cost','accumulativeCost'];
   constructor(private apollo: Apollo,private activatedroute:ActivatedRoute) { 
     this.upgrades=[
       {name:"Lock 2nd Generation",price:3000},
       {name:"Lock 3rd Generation", price:6000},
       {name:"Alarm 2nd Generation", price:5000},
       {name:"Alarm 3rd Generation", price:10000},
+      {name:"Anti-thief 1st Generation", price:5000},
+      {name:"Anti-thief 2nd Generation", price:10000},
+      {name:"Anti-thief 3rd Generation", price:15000},
+      {name:"Anti-thief 4th Generation", price:20000},
       {name:"GPS", price:500},
       {name:"Respray", price:1250},
       {name:"Spoiler", price:2000},
@@ -49,17 +55,18 @@ export class PriceguideDetailComponent implements OnInit {
       {name:"Frame", price:2000},
       {name:"Grille", price:1250},
       {name:"Hood", price:2000},
+      {name:"Neon", price:7000},
       {name:"Fenders", price:1500},
       {name:"Roof", price:2000},
       {name:"Engine", price:8000},
       {name:"Brakes", price:5000},
       {name:"Transmission", price:2000},
       {name:"Suspension", price:5000},
-      {name:"Wheels", price:750},
+      {name:"Wheels", price:1000},
       {name:"Liverys", price:3500},
       {name:"Plate", price:5000},
-      {name:"Turbo", price:9000},
-      {name:"Turbo", price:400},
+      {name:"Turbo", price:10000},
+      {name:"Xeon", price:400},
       {name:"Window Tint", price:200},
 
     ]
@@ -77,9 +84,19 @@ export class PriceguideDetailComponent implements OnInit {
     })
     .valueChanges.subscribe((result:any)=>{
       this.vehicle = result.data.getVehicle;
+      this.createTableDate();
       this.loading = result.loading;
       this.error = result.error;
     })
   }
-
+  createTableDate(){
+    this.maintance=[
+      {mileage:"1500 Mile",   cost: (this.vehicle.retail_price/30 *1), accumulativeCost:(this.vehicle.retail_price/30 * 1)},
+      {mileage:"3000 Mile",   cost: (this.vehicle.retail_price/30 *2), accumulativeCost:(this.vehicle.retail_price/30 * 3)},
+      {mileage:"4500 Mile",   cost: (this.vehicle.retail_price/30 *3), accumulativeCost:(this.vehicle.retail_price/30 * 6)},
+      {mileage:"6000 Mile",  cost: (this.vehicle.retail_price/30 *4), accumulativeCost:(this.vehicle.retail_price/30 * 10)},
+      {mileage:"7500 Mile",  cost: (this.vehicle.retail_price/30 *5), accumulativeCost:(this.vehicle.retail_price/30 * 15)},
+      {mileage:"10000 Mile", cost: (this.vehicle.retail_price/30 *6), accumulativeCost:(this.vehicle.retail_price/30 * 21)},
+    ]
+  }
 }
--- src/app/page/priceguide/priceguide.module.ts
+++ src/app/page/priceguide/priceguide.module.ts
@@ -10,6 +10,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
 
 import { PriceguideOverviewComponent } from './priceguide-overview/priceguide-overview.component';
 import { PriceguideClassListComponent } from './priceguide-class-list/priceguide-class-list.component';
@@ -27,7 +28,8 @@ import { PriceguideSearchComponent } from './priceguide-search/priceguide-search
     MatInputModule,
     MatProgressSpinnerModule,
     MatDividerModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatTableModule
   ]
 })
 export class PriceguideModule { }
