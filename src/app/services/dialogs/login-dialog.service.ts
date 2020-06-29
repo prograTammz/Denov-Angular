9b50d95d661e22809f644a4aa171e23a1931c48a
prograTammz <ahmedtamer1998@gmail.com>
2020-06-29 12:48:02 +0200

    modified the login dialog size

--- src/app/services/dialogs/login-dialog.service.ts
+++ src/app/services/dialogs/login-dialog.service.ts
@@ -13,7 +13,7 @@ export class LoginDialogService {
    }
   openModal(): void{
     const dialogRef = this.dialog.open(LoginComponent,{
-      width:'250px'
+      width:'80%'
     });
     dialogRef.afterClosed().subscribe(result => {
     });
