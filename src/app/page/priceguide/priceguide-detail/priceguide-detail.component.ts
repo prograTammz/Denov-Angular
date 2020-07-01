16b3d0c99ed3c4459bd59704f363546e1d653c85
prograTammz <ahmedtamer1998@gmail.com>
2020-07-01 15:40:46 +0200

    moved priceguide upgrade sdata  to service

--- src/app/page/priceguide/priceguide-detail/priceguide-detail.component.ts
+++ src/app/page/priceguide/priceguide-detail/priceguide-detail.component.ts
@@ -17,40 +17,7 @@ export class PriceguideDetailComponent implements OnInit {
   maintance: any[];
   displayedColumns: string[] = ['mileage','cost','accumulativeCost'];
   constructor(private PriceGuideService: PriceguideService,private activatedroute:ActivatedRoute) { 
-    this.upgrades=[
-      {name:"Lock 2nd Generation",price:3000},
-      {name:"Lock 3rd Generation", price:6000},
-      {name:"Alarm 2nd Generation", price:5000},
-      {name:"Alarm 3rd Generation", price:10000},
-      {name:"Anti-thief 1st Generation", price:5000},
-      {name:"Anti-thief 2nd Generation", price:10000},
-      {name:"Anti-thief 3rd Generation", price:15000},
-      {name:"Anti-thief 4th Generation", price:20000},
-      {name:"GPS", price:500},
-      {name:"Respray", price:1250},
-      {name:"Spoiler", price:2000},
-      {name:"Front Bumper", price:1600},
-      {name:"Side Skirt", price:1200},
-      {name:"Exhaust", price:1500},
-      {name:"Frame", price:2000},
-      {name:"Grille", price:1250},
-      {name:"Hood", price:2000},
-      {name:"Neon", price:7000},
-      {name:"Fenders", price:1500},
-      {name:"Roof", price:2000},
-      {name:"Engine", price:8000},
-      {name:"Brakes", price:5000},
-      {name:"Transmission", price:2000},
-      {name:"Suspension", price:5000},
-      {name:"Wheels", price:1000},
-      {name:"Liverys", price:3500},
-      {name:"Plate", price:5000},
-      {name:"Turbo non-sport", price:9000},
-      {name:"Turbo sport", price:11000},
-      {name:"Xeon", price:400},
-      {name:"Window Tint", price:200},
-
-    ]
+    this.upgrades=this.PriceGuideService.getUpgrades();
   }
 
   ngOnInit(): void {
